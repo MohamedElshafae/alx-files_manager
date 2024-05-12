cd6f34456ef81a9641342e105b3cd25b9b73d7da
MohamedElshafae <mohamedelshafae888@gmail.com>
2024-05-12 03:22:53 +0300

    connect to rides and make get and set and del operations

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,57 @@
 import redis from 'redis';
+
 class RedisClient {
   constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => {
-			console.log(err);
-		});
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis connection error:', err);
+    });
   }
-	isAlive() {
-		return this.client.connected;
-	}
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          console.error(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, exTime) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', exTime, (err, reply) => {
+        if (err) {
+          reject(err);
+          console.error(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          console.error(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 }
+
 const redisClient = new RedisClient();
+
 export default redisClient;
