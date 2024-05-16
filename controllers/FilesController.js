c99ce281548727155263347bea025d6d2c7a07e9
MohamedElshafae <mohamedelshafae888@gmail.com>
2024-05-16 09:58:03 +0300

    fix

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -159,6 +159,8 @@ const getFile = asyncHandler(async (req, res) => {
   if (file.type === 'folder') {
     throw new HttpError(400, 'A folder doesnt have content');
   }
+  if (!fs.existsSync(localPath))
+    throw new HttpError(404, 'Not found');
 
   const mimeType = mime.contentType(file.name);
   res.set('Content-Type', mimeType);
